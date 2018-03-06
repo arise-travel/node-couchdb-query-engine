47bd5b01db0e85db114891c5dfda62e289963ef6
jonassnellinckx <jo.snel@hotmail.com>
2018-03-06 20:38:55 +0100

    convert to filter instead of test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
+import ext_ from './engine/ext';
+import mongo_ from './engine/mongo';
 
-import ext_ from './engine/ext'
-import mongo_ from './engine/mongo'
-export { minimize } from './utils'
+export { minimize } from './utils';
 
-export default mongo_
+export default mongo_;
 
-export function test (a, q) {
-  return mongo_.test(a, q)
+export function parseCouchDBQuery(a, q) {
+
+    if (!q['selector']) {
+        throw new Error('Query needs a selector field');
+    }
+
+    const result = [];
+
+    for (const key in a) {
+        if (a.hasOwnProperty(key)) {
+            const value = a[key];
+
+            const test = mongo_.test(value, q);
+
+            if (test) {
+                result.push({
+                    key,
+                    value
+                });
+            }
+
+        }
+    }
+
+    let skip = q['skip'] | 0;
+    let limit = q['limit'] | result.length;
+
+
+    return result.slice(skip, limit);
 }
 
-export const ext = ext_
-export const mongo = mongo_
+export const ext = ext_;
+export const mongo = mongo_;
